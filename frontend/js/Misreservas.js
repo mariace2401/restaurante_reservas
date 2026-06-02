fac9d736e77c0b36b32b44ed7e2ad02b526ffb6b
aristizabal7m <s.aristizabal3@utp.edu.co>
2026-06-01 19:46:35 -0500

    small change

--- frontend/js/Misreservas.js
+++ frontend/js/Misreservas.js
@@ -1,6 +1,6 @@
 const API = window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1'
   ? 'http://127.0.0.1:8000'
-  : 'https://reservibe-tdr2.onrender.com';
+  : '';
 
 function logout() {
   localStorage.removeItem('rv_token');
